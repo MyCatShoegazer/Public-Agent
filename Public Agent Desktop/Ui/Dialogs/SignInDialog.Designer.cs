1af5051094ab3ca9da152e8a0b3e165902462d13
Александр Гелета <mycatshoegazer@outlook.com>
2020-02-27 15:55:39 +0300

    Created sign in form canvas.

---
+++ Public Agent Desktop/Ui/Dialogs/SignInDialog.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace PublicAgentDesktop.Ui.Dialogs
+{
+    partial class SignInDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loginLabel = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.loginTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.credentialsGroupBox = new System.Windows.Forms.GroupBox();
+            this.settingsGroupBox = new System.Windows.Forms.GroupBox();
+            this.languageLabel = new System.Windows.Forms.Label();
+            this.languageComboBox = new System.Windows.Forms.ComboBox();
+            this.systemStyleCheckBox = new System.Windows.Forms.CheckBox();
+            this.signInButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.signUpButton = new System.Windows.Forms.Button();
+            this.credentialsGroupBox.SuspendLayout();
+            this.settingsGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // loginLabel
+            // 
+            this.loginLabel.AutoSize = true;
+            this.loginLabel.Location = new System.Drawing.Point(26, 21);
+            this.loginLabel.Name = "loginLabel";
+            this.loginLabel.Size = new System.Drawing.Size(36, 13);
+            this.loginLabel.TabIndex = 0;
+            this.loginLabel.Text = "Login:";
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Location = new System.Drawing.Point(6, 47);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(56, 13);
+            this.passwordLabel.TabIndex = 1;
+            this.passwordLabel.Text = "Password:";
+            // 
+            // loginTextBox
+            // 
+            this.loginTextBox.Location = new System.Drawing.Point(68, 18);
+            this.loginTextBox.Name = "loginTextBox";
+            this.loginTextBox.Size = new System.Drawing.Size(146, 20);
+            this.loginTextBox.TabIndex = 2;
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(68, 44);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(146, 20);
+            this.passwordTextBox.TabIndex = 3;
+            this.passwordTextBox.UseSystemPasswordChar = true;
+            // 
+            // credentialsGroupBox
+            // 
+            this.credentialsGroupBox.Controls.Add(this.passwordLabel);
+            this.credentialsGroupBox.Controls.Add(this.passwordTextBox);
+            this.credentialsGroupBox.Controls.Add(this.loginLabel);
+            this.credentialsGroupBox.Controls.Add(this.loginTextBox);
+            this.credentialsGroupBox.Location = new System.Drawing.Point(12, 12);
+            this.credentialsGroupBox.Name = "credentialsGroupBox";
+            this.credentialsGroupBox.Size = new System.Drawing.Size(226, 73);
+            this.credentialsGroupBox.TabIndex = 4;
+            this.credentialsGroupBox.TabStop = false;
+            this.credentialsGroupBox.Text = "Credentials";
+            // 
+            // settingsGroupBox
+            // 
+            this.settingsGroupBox.Controls.Add(this.systemStyleCheckBox);
+            this.settingsGroupBox.Controls.Add(this.languageComboBox);
+            this.settingsGroupBox.Controls.Add(this.languageLabel);
+            this.settingsGroupBox.Location = new System.Drawing.Point(244, 12);
+            this.settingsGroupBox.Name = "settingsGroupBox";
+            this.settingsGroupBox.Size = new System.Drawing.Size(201, 73);
+            this.settingsGroupBox.TabIndex = 5;
+            this.settingsGroupBox.TabStop = false;
+            this.settingsGroupBox.Text = "Settings";
+            // 
+            // languageLabel
+            // 
+            this.languageLabel.AutoSize = true;
+            this.languageLabel.Location = new System.Drawing.Point(6, 21);
+            this.languageLabel.Name = "languageLabel";
+            this.languageLabel.Size = new System.Drawing.Size(58, 13);
+            this.languageLabel.TabIndex = 0;
+            this.languageLabel.Text = "Language:";
+            // 
+            // languageComboBox
+            // 
+            this.languageComboBox.FormattingEnabled = true;
+            this.languageComboBox.Location = new System.Drawing.Point(70, 18);
+            this.languageComboBox.Name = "languageComboBox";
+            this.languageComboBox.Size = new System.Drawing.Size(121, 21);
+            this.languageComboBox.TabIndex = 1;
+            // 
+            // systemStyleCheckBox
+            // 
+            this.systemStyleCheckBox.AutoSize = true;
+            this.systemStyleCheckBox.Location = new System.Drawing.Point(9, 45);
+            this.systemStyleCheckBox.Name = "systemStyleCheckBox";
+            this.systemStyleCheckBox.Size = new System.Drawing.Size(104, 17);
+            this.systemStyleCheckBox.TabIndex = 2;
+            this.systemStyleCheckBox.Text = "Use system style";
+            this.systemStyleCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // signInButton
+            // 
+            this.signInButton.Location = new System.Drawing.Point(12, 91);
+            this.signInButton.Name = "signInButton";
+            this.signInButton.Size = new System.Drawing.Size(226, 23);
+            this.signInButton.TabIndex = 6;
+            this.signInButton.Text = "Sign in";
+            this.signInButton.UseVisualStyleBackColor = true;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancelButton.Location = new System.Drawing.Point(363, 91);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(82, 23);
+            this.cancelButton.TabIndex = 7;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // signUpButton
+            // 
+            this.signUpButton.Location = new System.Drawing.Point(244, 91);
+            this.signUpButton.Name = "signUpButton";
+            this.signUpButton.Size = new System.Drawing.Size(113, 23);
+            this.signUpButton.TabIndex = 8;
+            this.signUpButton.Text = "Sign up";
+            this.signUpButton.UseVisualStyleBackColor = true;
+            // 
+            // SignInDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(453, 121);
+            this.Controls.Add(this.signUpButton);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.signInButton);
+            this.Controls.Add(this.settingsGroupBox);
+            this.Controls.Add(this.credentialsGroupBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "SignInDialog";
+            this.Text = "SignInDialog";
+            this.credentialsGroupBox.ResumeLayout(false);
+            this.credentialsGroupBox.PerformLayout();
+            this.settingsGroupBox.ResumeLayout(false);
+            this.settingsGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label loginLabel;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.TextBox loginTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.GroupBox credentialsGroupBox;
+        private System.Windows.Forms.GroupBox settingsGroupBox;
+        private System.Windows.Forms.ComboBox languageComboBox;
+        private System.Windows.Forms.Label languageLabel;
+        private System.Windows.Forms.CheckBox systemStyleCheckBox;
+        private System.Windows.Forms.Button signInButton;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Button signUpButton;
+    }
+}
