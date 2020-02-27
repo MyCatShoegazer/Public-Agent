b62ed743c8275380055d411c864ca8731a6e8208
Александр Гелета <mycatshoegazer@outlook.com>
2020-02-27 20:02:13 +0300

    Created GetMD5 string extension method.

---
+++ Public Agent Desktop/Utility/TypeExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace PublicAgentDesktop.Utility
+{
+    /// <summary>
+    ///    Предоставляет пользовательские методы расширения для
+    ///    базовых типов .NET Framework.
+    /// </summary>
+    public static class TypeExtensions
+    {
+        /// <summary>
+        ///     Получает MD5 сумму строки.
+        /// </summary>
+        /// <param name="source">Строка.</param>
+        /// <returns>Возвращает MD5 сумму строки.</returns>
+        public static string GetMD5(this string source)
+        {
+            var bytes = Encoding.Default.GetBytes(source);
+            using (var provider = new MD5CryptoServiceProvider())
+                bytes = provider.ComputeHash(bytes);
+
+            return Encoding.Default.GetString(bytes);
+        }
+    }
+}
