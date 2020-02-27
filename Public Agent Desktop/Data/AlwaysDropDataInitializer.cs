3b0cc8a62d16ba1372f980e026ee48414ed3e20d
Александр Гелета <mycatshoegazer@outlook.com>
2020-02-27 20:16:35 +0300

    Implemented data initializers.

---
+++ Public Agent Desktop/Data/AlwaysDropDataInitializer.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using PublicAgentDesktop.Data.Models;
+using PublicAgentDesktop.Utility;
+
+namespace PublicAgentDesktop.Data
+{
+    /// <summary>
+    ///     Удаляет и инициализирует базу данных каждый раз
+    ///     после перезапуска приложения.
+    ///     
+    ///     <para>
+    ///             Хорошо подходит для целей тестирования и отладки,
+    ///             когда при каждом запуске требуется иметь чистую базу данных.
+    ///     </para>
+    /// </summary>
+    public class AlwaysDropDataInitializer : DropCreateDatabaseAlways<PublicAgentDbContext>
+    {
+        /// <summary>
+        ///     Наполняет базу данных предустановленными данными.
+        /// </summary>
+        /// <param name="context">Контекст базы данных.</param>
+        protected override void Seed(PublicAgentDbContext context)
+        {
+            /*
+             * Группы привилегий по умолчанию.
+             */
+            var groups = new List<Group> 
+            {
+                new Group 
+                {
+                    Name = "Администраторы",
+                    Description = "Администраторы информационной системы. Обладают наивысшими правами."
+                },
+                new Group
+                {
+                    Name = "Пользователи",
+                    Description = "Пользователи информационной системы. Обладают стандартными правами."
+                },
+                new Group
+                {
+                    Name = "Гости",
+                    Description = "Гости информационной системы. Обладают исключительно правами только на чтение записей."
+                }
+            };
+
+            /*
+             * Администратор системы по умолчанию.
+             */
+            var root = new User
+            {
+                Login = "root",
+                Password = "1234".GetMD5(),
+                Email = "root@root.org",
+                Phone = "9990000",
+                LastName = "Root",
+                FirstName = "Root",
+                Group = groups.FirstOrDefault(g => g.Name == "Администраторы")
+            };
+
+            /*
+             * Добавление записей по умолчание в контекст.
+             */
+            context.Groups.AddRange(groups);
+            context.Users.Add(root);
+        }
+    }
+}
---
+++ Public Agent Desktop/Data/DropOnModelChangeDataInitializer.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using PublicAgentDesktop.Data.Models;
+using PublicAgentDesktop.Utility;
+
+namespace PublicAgentDesktop.Data
+{
+    /// <summary>
+    ///     Инициализатор контекста базы данных по умолчанию.
+    ///     Удаляет данные только в том случае, если одна из моделей
+    ///     изменилась со стороны программного кода.
+    /// </summary>
+    public class DropOnModelChangeDataInitializer : DropCreateDatabaseIfModelChanges<PublicAgentDbContext>
+    {
+        protected override void Seed(PublicAgentDbContext context)
+        {
+            /*
+             * Группы привилегий по умолчанию.
+             */
+            var groups = new List<Group>
+            {
+                new Group
+                {
+                    Name = "Администраторы",
+                    Description = "Администраторы информационной системы. Обладают наивысшими правами."
+                },
+                new Group
+                {
+                    Name = "Пользователи",
+                    Description = "Пользователи информационной системы. Обладают стандартными правами."
+                },
+                new Group
+                {
+                    Name = "Гости",
+                    Description = "Гости информационной системы. Обладают исключительно правами только на чтение записей."
+                }
+            };
+
+            /*
+             * Администратор системы по умолчанию.
+             */
+            var root = new User
+            {
+                Login = "root",
+                Password = "1234".GetMD5(),
+                Email = "root@root.org",
+                Phone = "9990000",
+                LastName = "Root",
+                FirstName = "Root",
+                Group = groups.FirstOrDefault(g => g.Name == "Администраторы")
+            };
+
+            /*
+             * Добавление записей по умолчание в контекст.
+             */
+            context.Groups.AddRange(groups);
+            context.Users.Add(root);
+        }
+    }
+}
